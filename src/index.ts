c0ff698bc3a2e95666ed7a085d462456aace8ec9
ToseebNadaf <toseebnadaf@gmail.com>
2025-03-01 00:51:09 +0530

    Created socket connection with redis

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,42 @@
 import express, { Application, Request, Response } from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import cors from "cors";
-import { router } from "./routes";
-
-dotenv.config();
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { setupSocket } from "./socket.js";
+import { createAdapter } from "@socket.io/redis-streams-adapter";
+import redis from "./config/redis.js";
+import { instrument } from "@socket.io/admin-ui";
+import { router } from "./routes/index.js";
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT!;
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+  },
+  adapter: createAdapter(redis),
+});
+
+instrument(io, {
+  auth: false,
+  mode: "development",
+});
+
+export { io };
+setupSocket(io);
 
 // * Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Chat_App Server");
+app.get("/", (req: Request, res: Response): void => {
+  res.send("It's working");
 });
 
-// * Routes
 app.use("/api", router);
 
-app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
-});
+server.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
---
+++ src/socket.ts
@@ -0,0 +1,27 @@
+import { Server, Socket } from "socket.io";
+
+interface CustomSocket extends Socket {
+  room?: string;
+}
+export function setupSocket(io: Server) {
+  io.use((socket: CustomSocket, next) => {
+    const room = socket.handshake.auth.room || socket.handshake.headers.room;
+    if (!room) {
+      return next(new Error("Invalid room"));
+    }
+    socket.room = room;
+    next();
+  });
+
+  io.on("connection", (socket: CustomSocket) => {
+    socket.join(socket.room!);
+
+    socket.on("message", async (data) => {
+      socket.emit("message", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected:", socket.id);
+    });
+  });
+}
